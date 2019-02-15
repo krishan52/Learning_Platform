b19bd1e0d1d3550d062af523e6064f78ef871e6a
Emiliano Mastragostino <emiliano.mastra@gmail.com>
2019-02-15 14:52:29 +0100

    Header and footer

--- public/js/main.js
+++ public/js/main.js
@@ -33,24 +33,24 @@ jQuery(document).ready(function( $ ) {
     $('#intro').css({ height: $(window).height() });
   }
 
-  // Initiate the wowjs animation library
-  new WOW().init();
-
-  // Initialize Venobox
-  $('.venobox').venobox({
-    bgcolor: '',
-    overlayColor: 'rgba(6, 12, 34, 0.85)',
-    closeBackground: '',
-    closeColor: '#fff'
-  });
-
-  // Initiate superfish on nav menu
-  $('.nav-menu').superfish({
-    animation: {
-      opacity: 'show'
-    },
-    speed: 400
-  });
+  // // Initiate the wowjs animation library
+  // new WOW().init();
+  //
+  // // Initialize Venobox
+  // $('.venobox').venobox({
+  //   bgcolor: '',
+  //   overlayColor: 'rgba(6, 12, 34, 0.85)',
+  //   closeBackground: '',
+  //   closeColor: '#fff'
+  // });
+  //
+  // // Initiate superfish on nav menu
+  // $('.nav-menu').superfish({
+  //   animation: {
+  //     opacity: 'show'
+  //   },
+  //   speed: 400
+  // });
 
   // Mobile Navigation
   if ($('#nav-menu-container').length) {
@@ -126,23 +126,23 @@ jQuery(document).ready(function( $ ) {
     }
   });
 
-  // Gallery carousel (uses the Owl Carousel library)
-  $(".gallery-carousel").owlCarousel({
-    autoplay: true,
-    dots: true,
-    loop: true,
-    center:true,
-    responsive: { 0: { items: 1 }, 768: { items: 3 }, 992: { items: 4 }, 1200: {items: 5}
-    }
-  });
-
-  // Buy tickets select the ticket type on click
-  $('#buy-ticket-modal').on('show.bs.modal', function (event) {
-    var button = $(event.relatedTarget);
-    var ticketType = button.data('ticket-type');
-    var modal = $(this);
-    modal.find('#ticket-type').val(ticketType);
-  })
+  // // Gallery carousel (uses the Owl Carousel library)
+  // $(".gallery-carousel").owlCarousel({
+  //   autoplay: true,
+  //   dots: true,
+  //   loop: true,
+  //   center:true,
+  //   responsive: { 0: { items: 1 }, 768: { items: 3 }, 992: { items: 4 }, 1200: {items: 5}
+  //   }
+  // });
+  //
+  // // Buy tickets select the ticket type on click
+  // $('#buy-ticket-modal').on('show.bs.modal', function (event) {
+  //   var button = $(event.relatedTarget);
+  //   var ticketType = button.data('ticket-type');
+  //   var modal = $(this);
+  //   modal.find('#ticket-type').val(ticketType);
+  // })
 
 // custom code
 
